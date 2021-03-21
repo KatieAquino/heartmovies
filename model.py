12b29f2c1ad5445e828351acd98e5ed49a1895b0
Hackbright Student <student@hackbrightacademy.com>
2021-03-20 23:10:14 -0700

    Creates movie and favoritemovies classes and tables.

--- model.py
+++ model.py
@@ -31,7 +31,36 @@ class User(db.Model):
     def __repr__(self):
         """Cleaner representation of user for ease of use."""
 
-        return f'<User id={self.id}, username={self.username}, email={self.email}'
+        return f'<User id={self.id}, username={self.username}, email={self.email}>'
 
+
+class Movie(db.Model):
+    """Movie in the HeartMovies database."""
+
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    poster = db.Column(db.String)
+    plot = db.Column(db.Text)
+
+
+    def __repr__(self):
+        """Cleaner representation of a movie for ease of use."""
+
+        return f'<Movie id={self.id}, title={self.title}>'
+
+
+class FavoriteMovie(db.Model):
+    """Favorited movie assigned to a user."""
+
+    __tablename__ = 'favorites'
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'))
+
+    user = db.relationship('User', backref='users')
+    movie = db.relationship('Movie', backref='movies')
 
 
