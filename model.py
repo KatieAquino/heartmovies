e64f5e509bc79cf00c6aebdbf32a5ddc663f0c2b
Hackbright Student <student@hackbrightacademy.com>
2021-03-20 22:52:11 -0700

    Creates user class and users table.

---
+++ model.py
@@ -0,0 +1,37 @@
+"""Database for HeartMovies"""
+
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """User in the HeartMovies database."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    username = db.Column(db.string(20), nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String(128))
+
+    # hash password for database security
+    def set_password(self, password):
+        """Hashes a users password for added security."""
+
+        self.password = generate_password_hash(password)
+    
+
+    def check_password(self, password):
+        """Checks a password entered to see if it matches the hashed password."""
+
+        return check_password_hash(self.password, password)
+
+
+    def __repr__(self):
+        """Cleaner representation of user for ease of use."""
+
+        return f'<User id={self.id}, username={self.username}, email={self.email}'
+
+
+
